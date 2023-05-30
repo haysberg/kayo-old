1cf7fbe0a763f80308f9e2df31b636e6cda904c0
Zorrino <pierrekouyoumdjian@hotmail.fr>
2023-05-30 16:05:08 +0200

    Connection to mongodb

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import os
 import logging
 import sys
-
+from pymongo import MongoClient
 import discord
 import dotenv
 
 # Initializing core objects
 dotenv.load_dotenv()
 bot = discord.Bot()
+
+def get_database():
+   db = MongoClient(os.getenv("MONGO_URI"))
+
+   return db['users']
+
+def insert_into_db(user_id, item):
+    dbname = get_database()
+    collection_name = dbname[user_id]
+    collection_name.insert_many([item])
 
 # Logging
 logger = logging.getLogger('discord')
@@ -34,5 +44,14 @@ async def hello(ctx, name: str = None):
 async def ping(ctx): # a slash command will be created with the name "ping"
     latency_ms = round(bot.latency * 1000)
     await ctx.respond(f"Pong! Latency is {latency_ms} ms")
+
+@bot.command(description="Subscribe the user to a league.")
+async def add(ctx):
+    item_1 = {
+    "league" : "trofor",
+    "match" : "2"
+    }
+    insert_into_db(str(ctx.author.id), item_1)
+    await ctx.respond(f"Added!")
 
 bot.run(os.getenv("DISCORD_TOKEN"))
